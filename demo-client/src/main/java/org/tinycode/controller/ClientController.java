1a87773861966af5ff4f632e6d8a9ec4449a83c0
Lyue <15081802906@163.com>
2023-09-18 15:24:43 +0800

    [add] 整合nacos 配置中心

--- demo-client/src/main/java/org/tinycode/controller/ClientController.java
+++ demo-client/src/main/java/org/tinycode/controller/ClientController.java
@@ -3,6 +3,8 @@ package org.tinycode.controller;
 import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,10 +17,17 @@ import org.tinycode.feign.DServerFeignService;
 @Slf4j
 @RestController
 @RequestMapping("/demo/client")
+@RefreshScope //支持Nacos的动态刷新
 public class ClientController {
 
 //    @Autowired
 //    private RestTemplate restTemplate;
+
+    @Value("${config.info}") // 从nacos中取
+    private String configInfo;
+
+    @Value("${config.common}") // 从nacos中取
+    private String configCommon;
 
     @Autowired
     private DServerFeignService dServerFeignService;
@@ -37,10 +46,25 @@ public class ClientController {
 //        return new ResponseEntity(msg, HttpStatus.OK);
 //    }
 
+    /**
+     * 服务间调用demo
+     * @return 调用端口号
+     */
     @GetMapping("/open/feign/demo/server")
     public ResponseEntity invokingServer() {
         log.info("this is openfeign invoking /open/feign/demo/server");
         DClientResponseData dClientResponseData = dServerFeignService.showClientPort("7001");
+        return new ResponseEntity(dClientResponseData, HttpStatus.OK);
+    }
+
+    /**
+     * nacos 注册中心demo
+     * @return 注册中心中配置的文件属性
+     */
+    @GetMapping("/nacos/config/info")
+    public ResponseEntity getNacosInfo() {
+        log.info("request url :/nacos/config/info");
+        DClientResponseData dClientResponseData =new DClientResponseData( " configInfo:" + configInfo + " configCommon:" + configCommon);
         return new ResponseEntity(dClientResponseData, HttpStatus.OK);
     }
 
